1f6b0dc63a6268e3ae82f752c3767b00ab1459bf
bbradson <76005458+bbradson@users.noreply.github.com>
2021-03-29 10:37:16 +0200

    update for BC7

--- DDSgenerator.ps1
+++ DDSgenerator.ps1
@@ -4,10 +4,10 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
     exit;
 }
 
-#Checking for ImageMagick
-if (!(Get-Command Magick -errorAction SilentlyContinue))
+#Checking for dependancies
+if (!(Get-Command $PSScriptRoot\dependancies\magick.exe -errorAction SilentlyContinue) -or !(Get-Command $PSScriptRoot\dependancies\texconv.exe -errorAction SilentlyContinue) -or !(Get-Command $PSScriptRoot\dependancies\texdiag.exe -errorAction SilentlyContinue))
 {
-    Write-Host "ImageMagick seems to not be installed. Get it from imagemagick.org, install it and try again." -ForegroundColor Yellow
+    Write-Host "Dependancies not found. Make sure to put the dependancies folder in the same place as the script." -ForegroundColor Yellow
     Start-Sleep 10
     return
 }
@@ -21,50 +21,91 @@ Out-File -FilePath C:\ddsgeneratortempcache.txt -InputObject $PSScriptRoot -Enco
 #Getting the images in the folder this script is located in, as well as its subfolders
 $images = Get-ChildItem -literalPath (get-item -literalPath (gc 'C:\ddsgeneratortempcache.txt')) -filter("*.??g") -Recurse | % { $_.FullName }
 
-#Delete the cache file of the workaround
-Remove-Item -LiteralPath 'C:\ddsgeneratortempcache.txt' -Force
-
-#This variable allows changing the amount of threads to use. Default is [int]$env:NUMBER_OF_PROCESSORS*2
-$MaxThreads = [int]$env:NUMBER_OF_PROCESSORS*2
+#This variable allows changing the amount of threads to use. Default is [int]$env:NUMBER_OF_PROCESSORS
+$MaxThreads = [int]$env:NUMBER_OF_PROCESSORS
 
 #Script with the ImageMagick operation that actually generates the DDS files
 $Scriptblock = {
-    param($png)
+    param($png,$scriptpath)
 
-    #Normally, with the following setting commented out, ImageMagick generates the maximum number of mip levels for zooming to a size of 1x1
-    #In theory, MipMaps increase file size by 33%, while increasing zooming performance and quality.
-    #Rimworld however, sadly, somewhat struggles with MipMaps. Things start disappearing among other issues. This setting here is used to disable MipMaps.
-    $MipMapSize = '-define','dds:mipmaps=0'
+    #valid texdiag results from [9] of the array
+    $format1 = "       format = B8G8R8A8_UNORM_SRGB"
+    $format2 = "       format = R8G8B8A8_UNORM_SRGB"
+    $format3 = "       format = B8G8R8X8_UNORM_SRGB"
+    #$format4 = "       format = B8G8R8A8_UNORM"
+    #$format5 = "       format = R8G8B8A8_UNORM"
+    #$format6 = "       format = B8G8R8X8_UNORM"
+
+    #Uncommenting the following disables MipMaps with texconv.
+    #This reduces filesize and memory usage by 25%, but also worsens zooming performance and quality.
+    #$MipMaps = '-m','1'
+
+    #Same as above, but ImageMagick
+    #$MipMapIM = '-define','dds:mipmaps=0'
     
-    #Output files get the same name as the input, with .png or .jpg replaced by .dds
-    $dds = $png -replace ".{4}$",'.dds'
-
     #Quoting. Not sure if necessary tbh
-    $ddsQuoted = '"' + $dds + '"'
     $pngQuoted = '"' + $png + '"'
 
-    #Checking for transparency
-    $opaque = Magick identify -format %[opaque] $pngQuoted
+    #Outputpath
+    $output = Split-Path -Path $png
+    #Outputfile
+    $dds = $png -replace ".{4}$",'.dds'
+    $ddsQuoted = '"' + $dds + '"'
 
-    #Fully opaque images get compressed with DXT1, others with DXT5
+    #Checking for transparency
+    $opaque = & $scriptpath\dependancies\magick.exe identify -format %[opaque] $pngQuoted
+
+    #Checking colorspace. texconv can't convert this, so it's necessary here
+    $texd = & $scriptpath\dependancies\texdiag.exe info $pngQuoted
+
+    #texconv compression. -y allows overwriting, -vflip flips to fulfil the unity requirement, -fixbc4x4 tries to fix unsupported texture dimensions, -o is the output
+    function futexconv { & $scriptpath\dependancies\texconv.exe $MipMaps $format -y -vflip -fixbc4x4 -o $output $pngQuoted }
+
+    #ImageMagick compression
+    function fuMagick { & $scriptpath\dependancies\magick.exe $pngQuoted -flip -define dds:compression=dxt1 $MipMapIM $ddsQuoted }
+
+    #Fully opaque images get compressed with DXT1/BC1, others with BC7. DXT1 has a 50% lower size than BC7, but if quality is important using BC7 everywhere is possible too.
     if ($opaque -eq 'True') {
-        $compression = 'dds:compression=dxt1'
+
+        #for B8G8R8A8_UNORM_SRGB etc convert to BC7_UNORM_SRGB
+        if (($texd[9] -eq $format1) -or ($texd[9] -eq $format2) -or ($texd[9] -eq $format3)){
+        #$format = '-f','BC1_UNORM_SRGB'
+
+        #texconv apparently can't properly compress to DXT1 with SRGB, so we use ImageMagick here.
+        #Why are all these compression tools from those big corporations so damn shit
+        fuMagick
+
+        #for B8G8R8A8_UNORM etc convert to BC7_UNORM. In this case, texconv works again.
+        } else {
+        $format = '-f','BC1_UNORM'
+        futexconv
+        }
     } else {
-        $compression = 'dds:compression=dxt5'
+
+        #for B8G8R8A8_UNORM_SRGB etc convert to BC7_UNORM_SRGB
+        if (($texd[9] -eq $format1) -or ($texd[9] -eq $format2)){
+        $format = '-f','BC7_UNORM_SRGB'
+        futexconv
+
+        #for B8G8R8A8_UNORM etc convert to BC7_UNORM
+        } else {
+        $format = '-f','BC7_UNORM'
+        futexconv
+        }
     }
-    Magick $pngQuoted -flip -define $compression $MipMapSize $ddsQuoted
 }
 
 #MultiThreading
 $RunspacePool = [runspacefactory]::CreateRunspacePool(1, $MaxThreads)
 $RunspacePool.Open()
 $jobs = New-Object System.Collections.ArrayList
+$filepath = get-item -literalPath (gc 'C:\ddsgeneratortempcache.txt')
 
 $images | ForEach {
 
     $PowerShell = [powershell]::Create()
     $PowerShell.RunspacePool = $RunspacePool
-    [void]$PowerShell.AddScript($ScriptBlock).AddArgument($_)
+    [void]$PowerShell.AddScript($ScriptBlock).AddArgument($_).AddArgument($filepath)
     $Handle = $PowerShell.BeginInvoke()
 
     $temp = [PSCustomObject]@{
@@ -86,6 +127,9 @@ while ($Handle.IsCompleted -contains $false) {
     }).Count)
 }
 
+#Delete the cache file of the workaround
+Remove-Item -LiteralPath 'C:\ddsgeneratortempcache.txt' -Force
+
 #Cleanup
 $return = $jobs | ForEach {
 
@@ -95,4 +139,12 @@ $return = $jobs | ForEach {
 
 }
 
+echo (“Successfully generated {0} files.” -f @($jobs | Where {
+    
+    $_.handle.iscompleted -eq ‘Completed’
+
+    }).Count)
+
 $jobs.clear()
+
+Start-Sleep 7
--- README.md
+++ README.md
@@ -3,12 +3,11 @@
 A script to quickly and easily generate DDS files for textures, with the goal of reducing load times, RAM usage and VRAM usage, as well as improving fps.
 Created for Rimworld, but this should work just fine for other games too.
 
-Makes use of ImageMagick, and as such ImageMagick has to be installed for this to work.
+Makes use of Microsoft texconv and texdiag as well as ImageMagick.
 Supported formats are JPG and PNG, but other formats supported by ImageMagick can easily be edited in when necessary too.
 
 General usage of this script is:
- - Install ImageMagick if not already installed
- - Place the script in a folder that contains pictures anywhere, including subfolders, and all their subfolders.
+ - Place the script and its dependancies folder in a folder that contains pictures anywhere, including subfolders, and all their subfolders.
  - Right-click
  - Run with powershell
  - ???
